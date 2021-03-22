1088e0b83ac4ec84da18d38cbb13cb596fe3d144
aschneiderGit <aimeschneider68@gmail.com>
2021-03-22 14:32:19 +0100

    post inscription done

--- src/main/java/fr/esiea/ex4A/ApiController.java
+++ src/main/java/fr/esiea/ex4A/ApiController.java
@@ -1,6 +1,11 @@
 package fr.esiea.ex4A;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class ApiController {
@@ -16,5 +21,21 @@ public class ApiController {
         System.out.println(user);
         System.out.println(userRepository.users.size());
         System.out.println(userRepository.users.get(userRepository.users.size()-1));
+    }
+
+    @GetMapping("api/matches")
+    String match(@RequestParam(name="userName") String username, @RequestParam(name="userCountry") String country) throws JSONException {
+        System.out.println(country);
+        System.out.println(username);
+        ArrayList<User> userMatch = userRepository.getByCountry(country, userRepository.getByUsername(username, userRepository.users));
+        JSONArray array = new JSONArray();
+        for (User user : userMatch) {
+            JSONObject item = new JSONObject();
+            item.put("name", user.username);
+            item.put("twitter", user.twitter);
+            array.put(item);
+        }
+        return array.toString() ;
+
     }
 }
